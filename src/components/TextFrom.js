1382bc8300451c50da136df57ba2e91afee9f37c
unknown <shobhan1rath@gmail.com>
2024-05-13 22:41:29 +0530

    2nd update 13th may

--- src/components/TextFrom.js
+++ src/components/TextFrom.js
@@ -1,7 +1,7 @@
 import React, {useState}  from 'react'
 
 export default function TextFrom(props) {
-    const [text, textCount ] = useState('Enter your text here');
+    const [text, textCount ] = useState('');
 
     const handleOnClick = () => {
        const thenewtext = text.toUpperCase();
@@ -21,18 +21,26 @@ export default function TextFrom(props) {
     <> <div>
         <h1>{props.heading}</h1>
     <div className="mb-3">
-              <textarea className="form-control" value={text} onChange={handleOnChange} id="MyBox" rows="5"></textarea>
+              <textarea className="form-control" value={text} onChange={handleOnChange} id="MyBox" rows="8"></textarea>
           </div>
           </div>
           <div className="row">
   <div className="col">
-    <button className="btn btn-primary mb-1" onClick={handleOnClick}>Convert Your Text</button>
+    <button className="btn btn-primary mx-2" onClick={handleOnClick}>Convert Your Text</button>
   </div>
   <div className="col">
-    <button className="btn btn-primary" onClick={handleOnClickLower}>Convert Your Text</button>
+    <button className="btn btn-primary " onClick={handleOnClickLower}>Convert Your Text</button>
   </div>
 </div>
-
+<div className="container my-3">
+  <h3>You Text Summary</h3>
+  <p>The number of characters in your text {text.length} and number of words are {text.split(" ").length}</p>
+  <p>Time for the user to study the wordsis in  {0.008 * text.split(" ").length} minutes</p>
+  <h2>
+    Preview
+  </h2>
+  <p>{text}</p>
+</div>
     </>
   )
 }
