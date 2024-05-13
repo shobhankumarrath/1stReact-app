9df5287d6f9830a488eaaaa7d8d2b359abe40e04
unknown <shobhan1rath@gmail.com>
2024-05-14 00:18:07 +0530

    Added a new button and function to validate json

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar(props) {
             <a className="nav-link active" aria-current="page" href="/">{props.homePage}</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">{props.aboutText}</a>
+            <a className="nav-link" href="./About.js">{props.aboutText}</a>
           </li>
         </ul>
         <form className="d-flex" role="search">
